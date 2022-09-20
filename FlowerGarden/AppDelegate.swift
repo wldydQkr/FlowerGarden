1fb8814be3f26d132706c60dc7ae422d203e6901
kdw <doowone8650@gmail.com>
2022-09-20 15:49:07 +0900

    테스트 dw

--- FlowerGarden/AppDelegate.swift
+++ FlowerGarden/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import NMapsMap
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,8 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        NMFAuthManager.shared().clientId = "oawdp0aaj8"
+
         return true
+
     }
 
     // MARK: UISceneSession Lifecycle
--- FlowerGarden/Scenes/MapViewController.swift
+++ FlowerGarden/Scenes/MapViewController.swift
@@ -1,17 +1,70 @@
 //
-//  MapViewController.swift
+//  ViewController.swift
 //  FlowerGarden
 //
 //  Created by 박지용 on 2022/09/13.
 //
-
 import UIKit
+import NMapsMap
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
+        
+        mapView.positionMode = .normal
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            print("위치 서비스 On 상태")
+            locationManager.startUpdatingLocation()
+            print(locationManager.location?.coordinate)
+
+            //현 위치로 카메라 이동
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
+            cameraUpdate.animation = .easeIn
+            mapView.moveCamera(cameraUpdate)
+
+            let marker = NMFMarker()
+            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+
+            marker.mapView = mapView
+        } else {
+            print("위치 서비스 Off 상태")
+        }
+
+        // ------------------------------마커 여러개 찍기 -----------------------------------------
+        DispatchQueue.global(qos: .default).async { [self] in
+            // 백그라운드 스레드
+            var markers = [NMFMarker]()
+            for index in 1...10 {
+                var i = Double(index) * 0.01
+                let marker = NMFMarker(position: NMGLatLng(lat: (self.locationManager.location?.coordinate.latitude)! + i ?? 0 , lng: locationManager.location?.coordinate.longitude ?? 0))
+//                marker.iconImage = ...
+//                marker.captionText = ...
+                markers.append(marker)
+            }
+
+            DispatchQueue.main.async {
+                // 메인 스레드
+                for marker in markers {
+                    marker.mapView = mapView
+                }
+            }
+        }
+        // ------------------------------마커 여러개 찍기 -----------------------------------------
 
     }
-
+    
 }
+
